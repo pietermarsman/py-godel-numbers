45eed5d929d4134cd385d744e3ea2c975f2d1ea4
Pieter Marsman <pietermarsman@gmail.com>
2024-07-17 21:36:46 +0200

    Degodelize numerical, sentential and predicate variables

--- tests/test_godel.py
+++ tests/test_godel.py
@@ -39,3 +39,21 @@ def test_godelize_variables(godel_number: int, variable: str) -> None:
 )
 def test_degodelize_constants(godel_number: int, constant: str) -> None:
     assert constant == degodelize(godel_number)
+
+
+@pytest.mark.parametrize(
+    ("godel_number", "variable"),
+    [
+        (13, "x"),
+        (17, "y"),
+        (19, "z"),
+        (13**2, "p"),
+        (17**2, "q"),
+        (19**2, "r"),
+        (13**3, "P"),
+        (17**3, "Q"),
+        (19**3, "R"),
+    ],
+)
+def test_degodelize_variables(godel_number: int, variable: str) -> None:
+    assert variable == degodelize(godel_number)
